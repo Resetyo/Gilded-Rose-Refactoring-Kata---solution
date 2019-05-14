c66d100f90142e6827bfd3102740034c2f898d59
Victor <ostorodonodor@gmail.com>
2019-05-14 20:05:48 +0700

    Refactor + Solution

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,49 +5,36 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
+      factor = 1
+
+      case item.name
+      when 'Sulfuras, Hand of Ragnaros'
+        next
+      when 'Aged Brie'
+        factor = -1
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        factor = -1
+        if item.sell_in <= 0
+          multiplicator = 0
+          item.quality = 0
+        elsif item.sell_in <= 5
+          multiplicator = 3
+        elsif item.sell_in <= 10
+          multiplicator = 2
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          multiplicator = 1
         end
+      when 'Conjured Item'
+        multiplicator = 2
       end
+
+      # standard goods
+      multiplicator ||= item.sell_in > 0 ? 1 : 2
+      item.quality -= (multiplicator * factor)
+      item.quality = 0 if item.quality < 0
+      item.quality = 50 if item.quality > 50
+
+      item.sell_in -= 1
     end
   end
 end
